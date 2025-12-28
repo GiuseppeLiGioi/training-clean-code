50f22898381aeb52601d493d24cc957f90cfe012
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-12-28 18:23:54 +0100

    add. logic for evaluating the security of a password

---
+++ logic/passwordStrength.ts
@@ -0,0 +1,46 @@
+import { PasswordStrength } from "@/types/passwordTypes";
+
+export const getPasswordStrength = (password: string): PasswordStrength => {
+  if (isWeakPassword(password)) return "weak";
+  else if (isMediumPassword(password)) return "medium";
+  else return "strong";
+};
+
+export const isStrongPassword = (password: string): boolean => {
+  return (
+    hasMinimunCharacters(password, 8) &&
+    hasNumbers(password) &&
+    hasSpecialCharacters(password)
+  );
+};
+
+export const isMediumPassword = (password: string): boolean => {
+  return hasMinimunCharacters(password, 6) && hasNumbers(password);
+};
+
+export const isWeakPassword = (password: string): boolean => {
+  return (
+    hasMinimunCharacters(password, 6) && hasOnlyLowercaseCharacters(password)
+  );
+};
+
+export const hasMinimunCharacters = (
+  password: string,
+  min: number
+): boolean => {
+  return password.length >= min;
+};
+
+export const hasOnlyLowercaseCharacters = (password: string): boolean => {
+  return password === password.toLowerCase();
+};
+
+export const hasNumbers = (password: string): boolean => {
+  const numbers = "0123456789";
+  return password.split("").some((c) => numbers.includes(c));
+};
+
+export const hasSpecialCharacters = (password: string): boolean => {
+  const specials = "@#[]<>£$%";
+  return password.split("").some((c) => specials.includes(c));
+};
